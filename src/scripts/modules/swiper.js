fb6289053d9f0e48896008c1684070413a3b7a38
sydyk120276 <sidikjan@mail.ru>
2025-04-10 14:02:54 +0600

    Предварительная вёрстка страницы inner

--- src/scripts/modules/swiper.js
+++ src/scripts/modules/swiper.js
@@ -44,69 +44,76 @@ if (infSliders.length) {
   });
 }
 
-const reviewSlider = document.querySelector(".review-clients-slider");
+document.addEventListener("DOMContentLoaded", () => {
+  const reviewSlider = document.querySelector(".review-clients-slider");
 
-if (reviewSlider) {
-  const reviewSwiper = new Swiper(reviewSlider, {
-    slidesPerView: 1,
-    breakpoints: {
-      767: {
-        slidesPerView: 2,
+  if (reviewSlider) {
+    const reviewSwiper = new Swiper(reviewSlider, {
+      slidesPerView: 1,
+      breakpoints: {
+        767: {
+          slidesPerView: 2,
+        },
+
+        1139: {
+          slidesPerView: 3,
+        },
       },
+      spaceBetween: 20,
 
-      1139: {
-        slidesPerView: 3,
+      pagination: {
+        el: ".swiper-pagination",
+        dynamicBullets: true,
+        clickable: true,
       },
-    },
-    spaceBetween: 20,
-
-    pagination: {
-      el: ".swiper-pagination",
-      dynamicBullets: true,
-      clickable: true,
-    },
-  });
-
-  const tabs = document.querySelectorAll(".review-clients-tabs button");
-
-  if (tabs) {
-    const reviewCards = document.querySelectorAll(".review-card");
-
-    const onClickFillSwiper = (evt) => {
-      const target = evt.target;
-      const type = target.dataset.type;
-
-      const active = document.querySelector(
-        ".review-clients-tabs button.active"
-      );
-
-      if (active === target) return;
-
-      active.classList.remove("active");
-      target.classList.add("active");
-
-      if (type !== "all") {
-        reviewCards.forEach((card) => {
-          card.dataset.type === type
-            ? card.classList.remove("hidden")
-            : card.classList.add("hidden");
-        });
-      } else {
-        reviewCards.forEach((card) => {
-          card.classList.contains("hidden")
-            ? card.classList.remove("hidden")
-            : null;
-        });
-      }
-
-      reviewSwiper.update();
-    };
-
-    tabs.forEach((tab) => {
-      tab.addEventListener("click", onClickFillSwiper);
     });
+
+    const tabs = document.querySelectorAll(".review-clients-tabs button");
+
+    if (tabs) {
+      const reviewCards = document.querySelectorAll(".review-card");
+
+      const onClickFillSwiper = (evt) => {
+        const target = evt.target;
+        const type = target.dataset.type;
+
+        const active = document.querySelector(
+          ".review-clients-tabs button.active"
+        );
+
+        if (active === target) return;
+
+        active.classList.remove("active");
+        target.classList.add("active");
+
+        if (type !== "all") {
+          reviewCards.forEach((card) => {
+            card.dataset.type === type
+              ? card.classList.remove("hidden")
+              : card.classList.add("hidden");
+          });
+        } else {
+          reviewCards.forEach((card) => {
+            card.classList.contains("hidden")
+              ? card.classList.remove("hidden")
+              : null;
+          });
+        }
+
+        reviewSwiper.update();
+      };
+
+      tabs.forEach((tab) => {
+        tab.addEventListener("click", onClickFillSwiper);
+      });
+    }
+
+    // Обновляем Swiper после изменения видимости слайдов
+    if (reviewSwiper) {
+      reviewSwiper.update(); // Обновляем размеры и пагинацию
+    }
   }
-}
+});
 
 const sliders = document.querySelectorAll(".main-slider");
 
